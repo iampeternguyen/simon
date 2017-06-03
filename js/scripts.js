d479a7012090ce0bde47779c55f09d58c059b228
iampeternguyen <ptnguyen1987@gmail.com>
2017-06-02 23:29:43 -0400

    random combo picker and color changer added

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var combo = [];
+
+function addCombo () {
+  combo.push(Math.floor(Math.random() * 4 + 1));
+}
+
+function playCombo () {
+
+}
+
+// colors for button press
+// $(document).ready(function () {
+//   $("#btn1").css("background-color", "#FFEA92");
+//   $("#btn2").css("background-color", "#FF8E76");
+//   $("#btn3").css("background-color", "#86AAAD");
+//   $("#btn4").css("background-color", "#A17184");
+// });
